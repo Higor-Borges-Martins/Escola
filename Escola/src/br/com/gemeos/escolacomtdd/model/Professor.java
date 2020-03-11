860c4eae6455a9dbc2e3ffcbbabefe31c6cb5db9
Hiury Borges Martins <hiurymartins@si.fiponline.edu.br>
2020-03-11 14:54:25 -0300

    Trabalho com alulo e professor prontos

---
+++ Escola/src/br/com/gemeos/escolacomtdd/model/Professor.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gemeos.escolacomtdd.model;
+
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Pessoal
+ */
+@Entity
+@PrimaryKeyJoinColumn(name = "idPessoa")
+public class Professor extends Pessoa{
+
+    @Column(length = 7)
+    private double salario;
+
+    public Professor() {
+        super();
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Professor{" + "salario=" + salario + '}';
+    }
+}
